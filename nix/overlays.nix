06d657c5ed514ee65391a726734f74cceefb168f
Sigmanificient <edhyjox@gmail.com>
2024-08-25 05:37:40 +0200

    Deduce the version number from the CHANELOG file

--- nix/overlays.nix
+++ nix/overlays.nix
@@ -5,8 +5,21 @@ self: final: prev: {
       (_: pprev: {
         qtile = (pprev.qtile.overrideAttrs (old: let
           flakever = self.shortRev or "dev";
+
+          releases = (builtins.filter
+            (x: !builtins.isList x && prev.lib.strings.hasPrefix "Qtile" x)
+            (builtins.split "\n" (builtins.readFile ../CHANGELOG)));
+
+          # the 0th element is the template
+          current-release-title = builtins.elemAt releases 1;
+
+          symver = builtins.head
+            (builtins.match
+              "Qtile ([0-9.]+), released ([0-9-]+):"
+              current-release-title);
+
         in {
-          version = "0.0.0+${flakever}.flake";
+          version = "${symver}+${flakever}.flake";
           # use the source of the git repo
           src = ./..;
           # for qtile migrate, not in nixpkgs yet
